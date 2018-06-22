1b59a2c57b87c4ff0c242219829e589134c41b1c
largeMing <aming_zz@163.com>
2018-06-22 10:47:19 +0800

    App.vue data no return

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Detail from '@/pages/detail/detail'
 import Home from '@/pages/home/home'
 Vue.use(Router)
